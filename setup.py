6bc8a5da5bfeaa3f958132aa3396c5ab315651f4
Richard Vaughan <richard.vaughan@xuvasi.com>
2019-02-22 17:47:50 +0000

    Version number bump for the library.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="wrc_livetiming",
-    version='0.0.1',
+    version='0.0.2',
     py_modules=['wrc_livetiming'],
     install_requires=[
         'Click',
